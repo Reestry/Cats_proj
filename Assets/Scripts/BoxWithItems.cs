cd45a7e96ad1bbf0eea4e756620f6b4e3fb3a9ed
Alexey <kurash.lexa@yandex.ru>
2025-12-24 11:04:05 +0300

    Minor bug fixes

--- Assets/Scripts/BoxWithItems.cs
+++ Assets/Scripts/BoxWithItems.cs
@@ -1,17 +1,11 @@
-using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using Player;
 using UnityEngine;
-using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
 public class BoxWithItems : MonoBehaviour, IInteractableItem<Rigidbody>
-
-
 {
-
-    
     [SerializeField] private ItemList _itemList;
 
     private List<Rigidbody> _items;
@@ -21,26 +15,25 @@ public class BoxWithItems : MonoBehaviour, IInteractableItem<Rigidbody>
         _items = new List<Rigidbody>(_itemList.ItemLists);
     }
 
-
-
     public Rigidbody Interact()
     {
-        try
-        {
+        
+        if (CheckItem())
             return GetItem();
-        }
-        catch
-        {
-            Debug.Log("Ничего нет :((");
-            DeleteBox();
-            return null;
-        }
+
+        Debug.Log("Ничего нет :((");
+        DeleteBox();
+        return null;
     }
 
+    private bool CheckItem()
+    {
+        return _items.Count > 0;
+    }
 
     private Rigidbody GetItem()
     {
-        if (_items.Count == 0)
+        if (!CheckItem())
             return null;
         
         var item = _items[Random.Range(0, _items.Count)];
@@ -48,10 +41,9 @@ public class BoxWithItems : MonoBehaviour, IInteractableItem<Rigidbody>
         var obj = Instantiate(item);
         obj.transform.position = transform.position;
         return obj;
-
     }
 
-    public void DeleteBox()
+    private void DeleteBox()
     {
         var scale = transform.localScale + new Vector3(0.1f, 0.1f, 0.1f);
         transform.DOScale(scale, 0.1f).SetEase(Ease.Flash).OnComplete(() =>
